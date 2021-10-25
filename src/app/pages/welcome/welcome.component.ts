7dbf28336dbd7f142b1713ecced54440a607fe54
duchuynhgleadsfe <duc.huynh@gleads.vn>
2021-10-25 14:08:46 +0700

    remove mock data

--- src/app/pages/welcome/welcome.component.ts
+++ src/app/pages/welcome/welcome.component.ts
@@ -13,13 +13,6 @@ import {LoadingStatus, TodosState} from "./state/todos.reducer";
 })
 export class WelcomeComponent implements OnInit {
 
-  todos: Todo[] = [
-    {id: 1, title: 'Demo Ngrx 1', status: 'Processing'},
-    {id: 2, title: 'Demo Ngrx 2', status: 'Done'},
-    {id: 3, title: 'Demo Ngrx 3', status: 'Pending'},
-    {id: 4, title: 'Demo Ngrx 4', status: 'Done'},
-  ];
-
   todos$: Observable<Todo[]> = this.store.select((state) => state.todos.list);
 
   loadingTodosStatus$: Observable<LoadingStatus> = this.store.select((state) => state.todos.loadingListStatus);
